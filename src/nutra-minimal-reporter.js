6662a69f73eb5f0a3c8b16fa956e7d9d7240890f
Marceli.no <me@marceli.no>
2016-07-20 22:36:24 -0400

    fix: Ensure success and failure messages are accurate
    Previously, the system just printed "All tests have passed."
    regardless of the outcome. This commit distinguishes between
    success and failure.

--- src/nutra-minimal-reporter.js
+++ src/nutra-minimal-reporter.js
@@ -3,34 +3,37 @@ import Color from 'colors/safe'
 const reporter = (events, system, opts) => {
     events.onLoad = () => {}
     events.onFrameworkExecution = framework => {
-        try {
-            const minimalReporter =  {
-                specDone: function(result) {
-                    if (result.failedExpectations.length > 0) {
-                        console.log(Color.red('Spec: "' + result.description + '", ' + result.status))
-                    }
-                    for (var i = 0; i < result.failedExpectations.length; i++) {
-                        console.log(Color.red(result.failedExpectations[i].stack))
-                        system.handleError(new Error(''))
-                    }
-                },
-                suiteDone: function(result) {
-                    if (result.failedExpectations.length > 0) {
-                        console.log(Color.red('Suite: "' + result.description + '"'))
-                    }
-                    for (var i = 0; i < result.failedExpectations.length; i++) {
-                        console.log(Color.red(result.failedExpectations[i].stack))
-                        system.handleError(new Error(''))
-                    }
-                },
-                jasmineDone: function() {
-                    console.log(Color.green('All tests passed.'))
+        let errors = 0
+        const minimalReporter =  {
+            specDone: function(result) {
+                if (result.failedExpectations.length > 0) {
+                    console.log(Color.red('Spec: "' + result.description + '", ' + result.status))
                 }
+                for (var i = 0; i < result.failedExpectations.length; i++) {
+                    console.log(Color.red(result.failedExpectations[i].stack))
+                    errors++
+                }
+            },
+            suiteDone: function(result) {
+                if (result.failedExpectations.length > 0) {
+                    console.log(Color.red('Suite: "' + result.description + '"'))
+                }
+                for (var i = 0; i < result.failedExpectations.length; i++) {
+                    console.log(Color.red(result.failedExpectations[i].stack))
+                    errors++
+                }
+            },
+            jasmineDone: function() {
+                const message = 'Tests have'
+                const outcome = ( errors ? 'failed.' : 'passed!' )
+                const colored = ( errors ?
+                    Color.red(message) + ' ' + Color.red(outcome) :
+                    Color.green(message) + ' ' + Color.green(outcome)
+                )
+                console.log(colored)
             }
-            framework.addReporter(minimalReporter)
-        } catch (e) {
-            console.log(e)
         }
+        framework.addReporter(minimalReporter)
     }
     events.onExit = () => {}
 }
